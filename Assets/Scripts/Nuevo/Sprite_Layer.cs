8ba6913912176ea4c523bee0c816ef3eb9d67318
alanh1213 <56370969+alanh1213@users.noreply.github.com>
2020-12-13 22:09:27 -0300

    Added procedural shadows
    Added script for the automatization of shadows generation in a procedural way and removed old manually placed shadows from some prefabs.

---
+++ Assets/Scripts/Nuevo/Sprite_Layer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite_Layer : MonoBehaviour
+{
+    private SpriteRenderer sprite_Renderer;
+    [SerializeField] private SpriteRenderer parentSpriteRenderer;
+    [SerializeField] private bool es_Brazo = false;
+    private Transform spriteDetector;
+
+    private void Awake()
+    {
+        sprite_Renderer = GetComponent<SpriteRenderer>();
+        spriteDetector = gameObject.transform.Find("SpriteDetector");
+    }
+    private void Update()
+    {
+        if(!es_Brazo)sprite_Renderer.sortingOrder = -(int) spriteDetector.transform.position.y;
+        else sprite_Renderer.sortingOrder = parentSpriteRenderer.sortingOrder;
+    }
+}
